a781875e571be20fe81d076a4a24f536717f1bb9
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-01-13 12:39:58 +0530

    example program

---
+++ firstprog.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int favourite_number;
+
+cout<<"Enter your favourite number between 1 to 100:";
+
+cin>> favourite_number;
+
+cout<<"Amazing!! Thats my favourite number too!"<<endl;
+
+return 0;
+
+}
